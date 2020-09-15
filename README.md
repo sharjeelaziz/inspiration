14faf1d54ab845dfd9dc7c13a5bf227d90804991
sharjeelaziz <sharjeel.aziz@gmail.com>
2020-09-15 11:48:30 -0400

    Initial import.
    Signed-off-by: sharjeelaziz <sharjeel.aziz@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# inspiration
+# Inspiration
+Something to put my old monitor to good use.
+
+```
+pip3 install -r requirements.txt
+python3 servequotes.py
+```
+
+Navigate to http://127.0.0.1:5000
+
+## Thank you for the pictures
+* [Jiarong Deng](https://www.pexels.com/@visionmonkeyandy)
+* [Chris Schippers](https://www.pexels.com/@chris-schippers)
+* [Paul Basel](https://www.pexels.com/@paul-basel-903490)
+* [Maxime Francis](https://www.pexels.com/@maxfrancis)
+* [Craig Adderley](https://www.pexels.com/@thatguycraig000)
+* [Furknsaglam](https://www.pexels.com/@furknsaglam-1596977)
+
---
+++ servequotes.py
@@ -0,0 +1,41 @@
+import webbrowser
+import json
+from flask import Flask, render_template, abort
+import requests
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def wisdom():
+    quoteList = get_random_quote()
+    if not quoteList:
+        error = 'Error loading quotes'
+        return render_template('index.html', error=error), 500
+
+    return render_template('index.html', quote=quoteList[0][0], author=quoteList[0][1],
+        quote1=quoteList[1][0], author1=quoteList[1][1],
+        quote2=quoteList[2][0], author2=quoteList[2][1],
+        quote3=quoteList[3][0], author3=quoteList[3][1],
+        quote4=quoteList[4][0], author4=quoteList[4][1],
+        quote5=quoteList[5][0], author5=quoteList[5][1])
+
+
+def get_random_quote():
+    f = open('quotes.json', )
+    data = json.load(f)
+    quoteList = []
+
+    try:
+        for x in range(6):
+            y = random.choice(data['quotes'])
+            quoteList.append((y['text'], y['author']))
+    except IndexError:
+        print("nothing found")
+
+    f.close()
+    return quoteList
+
+if __name__ == "__main__":
+    app.run()
+
